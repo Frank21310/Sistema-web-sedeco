5518d5bddc75c0f152cee98a758468c37257746a
Francisco Daniel Santaella Ruiz <45299753+Frank21310@users.noreply.github.com>
2024-07-11 11:34:04 -0600

    new adds a configurate for user transporte, auth, validate, ctrol

---
+++ app/Http/Controllers/Transportes/EntradaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Transportes;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EntradaController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Transportes/VehiculosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Transportes;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VehiculosController extends Controller
+{
+    //
+}
